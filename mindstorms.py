8e2673771143a0318de26b18f56f64d5c3476350
Yash Agarwal <yashagl1620@gmail.com>
2018-01-23 02:18:40 +0530

    3rd day

--- mindstorms.py
+++ mindstorms.py
@@ -1,18 +1,23 @@
 import turtle
-def draw_square():
+
+def draw_square(turt):
+    for i in range(1,5):
+        turt.forward(100)
+        turt.right(90)
+
+def draw_art():
     window=turtle.Screen()
     window.bgcolor("red")
     brad=turtle.Turtle()
-    brad.speed(2)
-    brad.color("green")
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    brad.speed(0)
+    brad.color("yellow")
+    for i in range(0,180):
+        draw_square(brad)
+        brad.right(2)
+    #angie=turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("blue")
+    #angie.circle(100)
     window.exitonclick()
-
-draw_square()
+    
+draw_art()
---
+++ send_message.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "AC6a8c0fb983bad3b144e3eed8c9185cac"
+# Your Auth Token from twilio.com/console
+auth_token  = "21c0d45e8ce414dd069a060192095a79"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+918004980177", 
+    from_="+18562494816",
+    body="Hello from Python!")
+
+print(message.sid)
